73ecd8cf83834afa16d5af704a96848bfcc71cee
majedur rahman <youremail@yourdomain.com>
2024-04-23 14:35:36 +0600

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.myapplication;
 
-
-
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,31 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listview=findViewById(R.id.listviewID);
+        listview = findViewById(R.id.listviewID);
 
-        name= getResources().getStringArray(R.array.playerList);
+        name = getResources().getStringArray(R.array.playerList);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.demo,R.id.textViewID, name);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.demo, R.id.textViewID, name);
         listview.setAdapter(adapter);
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String value =name[i];
-                Toast.makeText(MainActivity.this,value,Toast.LENGTH_LONG).show();
-                if(i==0)
-                {
-                    Intent intent=new Intent(MainActivity.this,messi.class);
-                    startActivity(intent);
-                }else if(i==1){
-                    Intent intent=new Intent(MainActivity.this,messi.class);
-                    startActivity(intent);
-                }
+                String value = name[i];
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
 
-
+                // Start the messi activity and pass the selected player's name as an extra
+                Intent intent = new Intent(MainActivity.this, messi.class);
+                intent.putExtra("playerName", value);
+                startActivity(intent);
             }
-
         });
-
     }
 }
--- app/src/main/java/com/example/myapplication/messi.java
+++ app/src/main/java/com/example/myapplication/messi.java
@@ -1,20 +1,29 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.ComponentActivity;
+import androidx.activity.OnBackPressedCallback;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class messi extends AppCompatActivity {
+public class messi extends ComponentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_messi);
+
+        // Retrieve the player's name from the Intent extra
+        String playerName = getIntent().getStringExtra("playerName");
+
+        // Find the TextView and set the player's name
+        TextView playerTextView = findViewById(R.id.textViewPlayer);
+        playerTextView.setText("This is " + playerName);
+
+        // Apply padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
